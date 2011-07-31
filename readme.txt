18a956cc55eb56d6c64235003e7f8203562fb123
thepoet <thepoet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-31 16:14:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wa-plurk-updater/tags/1.0.7@417571 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Plugin Name ===
+Contributors: thepoet
+Donate link: http://diwafernandez.com/blog/notes/plugin-wa-plurk/
+Tags: plurk, post
+Requires at least: 2.9
+Tested up to: 3.2.1
+Stable tag: 3.0.5
+
+This a simple plugin that sends any updates from your site to your Plurk account.
+
+== Description ==
+
+Posts any updates to your plurk account using the Plurk API. This plugin is inspired by the Twitter Updater plugin. Any comment and/or suggestion will be appreciated.
+
+
+== Installation ==
+
+1. Upload wa_plurk folder to the /wp-content/plugins/ directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to Tools, Plurk Updater to set your Plurk account and other choices then that's it.
+
+
+== Frequently Asked Questions ==
+
+= Do I need a Plurk Account =
+
+Yes, you need an account for where it will be posted
+
+
+== Screenshots ==
+
+1. Screenshot of the wa plurk options in wp-admin.
+
+== Changelog ==
+
+= 1.0.7 =
+* updated a few codes that might have a problem on posting on plurk.
+
+= 1.0.6 =
+* Added an option to removed saved plurk account from the option.
+
+= 1.0.5 =
+* Added a notice if the plurk account is valid.
+
+== Upgrade Notice ==
---
+++ wa_plurk.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: Wa Plurk Updater Plugin
+Plugin URI: http://wordpress.org/extend/plugins/wa-plurk-updater/
+Description: A wordpress plugin that posts any published post to your plurk account with an option to set what kind of text you want to be posted in Plurk.
+Version: 1.0.6
+Author: Diwa G. Fernandez
+Author URI: http://diwafernandez.com
+License: GPLv2
+*/
+/* Copyright 2011 Diwa Fernandez (email: axeli at gmail.com)
+
+This program is free software; you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details. 
+
+You should have received a copy of the GNU General Public License 
+along with this program; if not, write to the Free Software 
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
+*/
+define('WAPLURK_VERSION', '1.0.1');
+define ('WAPLURK_API_KEY', 'xtGYl72cAySSAYwyZQNWB2D86ty21l1Y'); // api key to connect to PLURK API *DO NOT DELETE*
+
+// ADMIN PANEL - under Manage menu
+function df_addWaPlurkAdminPages() {
+    if (function_exists('add_management_page')) {
+		 add_management_page('Plurk Updater', 'Plurk Updater', 8, __FILE__, 'df_Plurk_manage_page');
+    }
+ }
+
+function df_Plurk_manage_page() {
+    include(dirname(__FILE__).'/admin/admin.php');
+}
+
+require(dirname(__FILE__).'/api/plurk_api.php');
+
+function df_doPlurkAPIPost($psay,$ptext){
+global $plurk;
+//check if user login details have been entered on admin page
+	$thisLoginDetails = get_option('plurklogin_encrypted');
+	if ($thisLoginDetails != '') {
+		//$pairs = explode("&", $str)
+		$plurk = new plurk_api();
+		$pairs = explode("[]",base64_decode($thisLoginDetails));
+		$username = $pairs[0];
+		$password = $pairs[1];
+		$api_key = WAPLURK_API_KEY; // api key to connect to PLURK API *DO NOT DELETE*
+		$plurk->login($api_key, $username, $password);
+		$disName = $profile->user_info->display_name;
+		$plurk->add_plurk('en', $psay, $ptext);
+		$plurk->logout();
+	}
+}
+
+if (!function_exists('wa_plurk')) {
+	function wa_plurk($post_ID) {
+	   $thisposttitle = $_POST['post_title'];
+	   $thispostlink = get_permalink($post_ID);
+	   $sentence = '';
+	/*if (($get_post_info->post_status == 'publish' || $_POST['publish'] == 'Publish') && ($_POST['prev_status'] == 'draft' || $_POST['original_post_status'] == 'draft' || $_POST['prev_status'] == 'pending' || $_POST['original_post_status'] == 'pending')) {*/
+	//is new post
+	if($_POST['action'] !== 'autosave'){
+
+		if($get_post_info->post_status == 'publish' || $_POST['publish'] == 'Publish'){
+			// publish new post
+			if(get_option('newpost-published-update') == '1'){
+				$sentence = get_option('newpost-published-text');
+				$say = get_option('newpost-published-say');
+				if(get_option('newpost-published-showlink') == '1'){
+					$thisposttitle = $thispostlink . ' ( ' . $thisposttitle . ' )';
+				}
+				$sentence = str_replace ( '#title#', $thisposttitle, $sentence);
+			}
+		}else if($_POST['action'] == 'post'){
+			// create new post
+			if(get_option('newpost-created-update') == '1'){
+				$sentence = get_option('newpost-created-text');
+				$say = get_option('newpost-created-say');
+				$sentence = str_replace ( '#title#', $thisposttitle, $sentence);
+			}
+		}else{
+			// edit new post
+			if(get_option('newpost-edited-update') == '1'){
+				$sentence = get_option('newpost-edited-text');
+				$say = get_option('newpost-edited-say');
+				$sentence = str_replace ( '#title#', $thisposttitle, $sentence);
+			}
+		}
+	}else{
+		// is old post
+		if(get_option('oldpost-edited-update') == '1'){
+			$sentence = get_option('oldpost-edited-text');
+			$say = get_option('oldpost-edited-say');
+			if(get_option('oldpost-edited-showlink') == '1'){
+				$thisposttitle = $thispostlink . ' ( ' . $thisposttitle . ' )';
+			}
+			$sentence = str_replace ( '#title#', $thisposttitle, $sentence);
+		}
+	}
+		
+		if($sentence != ''){
+			df_doPlurkAPIPost($say,$sentence);
+		}
+	
+	}
+}
+// Removes information when plugin is deactivated
+function wa_plurk_remove () {
+	delete_option('waplurkInitialised');
+	delete_option('plurklogin');
+	delete_option('plurklogin_encrypted');
+}
+//HOOKIES
+add_action ( 'save_post', 'wa_plurk');
+
+add_action('admin_menu', 'df_addWaPlurkAdminPages');
+
+/* Runs on plugin deactivation*/
+register_deactivation_hook( __FILE__, 'wa_plurk_remove' );
+
+?>
